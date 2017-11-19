19d4645501381269434d1c6b8fc5fc3fcf48f6be
douxing <douxing1983@163.com>
2017-11-20 00:24:08 +0800

    partly init request.py

--- pbft/message/request.py
+++ pbft/message/request.py
@@ -1,5 +1,9 @@
+from ..types import Reqid
 from .types import MessageTag
 from .base_message import BaseMessage
 
 class Request(BaseMessage):
-    pass
+    def __init__(self,
+                 node_type:int, index:int,
+                 reqid:Reqid):
+        pass
